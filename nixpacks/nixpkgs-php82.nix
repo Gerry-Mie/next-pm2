a57981837517349cbbd104cb4ff2e7585a424968
gerrymie <gerrymie30.l@gmail.com>
2024-08-15 02:53:49 +0800

    update nix

--- nixpacks/nixpkgs-php82.nix
+++ nixpacks/nixpkgs-php82.nix
@@ -1,7 +1,8 @@
-   { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.09.tar.gz") {} }:
-   pkgs.mkShell {
-     buildInputs = [
-       pkgs.php82
-       # Add other dependencies as needed
-     ];
-   }
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.05.tar.gz") {} }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.php82
+    pkgs.composer
+    # add other dependencies
+  ];
+}
